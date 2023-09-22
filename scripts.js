14a195d0ee2a819315b2db4c2a44fd604d8088f0
k00ya <133164751+k00ya@users.noreply.github.com>
2023-09-21 19:37:56 -0700

    eliminated bloat and unneeded repetitive code

--- scripts.js
+++ scripts.js
@@ -1,10 +1,8 @@
-let isButtonClicked = false;  // Initialize a flag variable
+let isButtonClicked = false;
 
 window.onload = function() {
     alert("Congratulations! You won $6.0 million dollars!");
 };
-
-const video = document.getElementById('backgroundVideoo');
 
 const videos = {
     square1: document.getElementById('backgroundVideoo'),
@@ -12,38 +10,45 @@ const videos = {
     square3: document.getElementById('backgroundVideo3')
 };
 
+function playMedia(video, audio) {
+    video.style.opacity = '1';
+    video.play();
+    
+    let playPromise = audio.play();
+    
+    if (playPromise !== undefined) {
+        playPromise.then(_ => {}).catch(error => {
+            console.error("Playback failed: ", error);
+        });
+    }
+}
+
+function pauseMedia(video, audio) {
+    video.style.opacity = '0';
+    video.pause();
+    
+    audio.pause();
+    audio.currentTime = 0;
+}
+
 ['square1', 'square2', 'square3'].forEach((squareId, index) => {
     const square = document.getElementById(squareId);
     const audio = document.getElementById(`audio${index + 1}`);
     const video = videos[squareId];
 
     square.addEventListener('mouseover', function() {
-        if (isButtonClicked) {  // Check if the button has been clicked
-            Object.values(videos).forEach(v => {
-                v.style.opacity = '0';
-                v.pause();
-            });
-            video.style.opacity = '1';
-            video.play();
-            
-            let playPromise = audio.play();
-            
-            if (playPromise !== undefined) {
-                playPromise.then(_ => {
-                }).catch(error => {
-                    console.error("Playback failed: ", error);
-                });
-            }
+        if (isButtonClicked) {
+            playMedia(video, audio);
+            console.log('Mouseover event triggered.');
+            square.style.transform = 'translate(-50%, -50%) scale(1.1)';
         }
     });
 
     square.addEventListener('mouseout', function() {
-        if (isButtonClicked) {  // Check if the button has been clicked
-            video.style.opacity = '0';
-            video.pause();
-
-            audio.pause();
-            audio.currentTime = 0;
+        if (isButtonClicked) {
+            pauseMedia(video, audio);
+            console.log('Mouseout event triggered.');
+            square.style.transform = 'translate(-50%, -50%) scale(1)';
         }
     });
 });
@@ -57,36 +62,5 @@ document.getElementById('glow-on-hover').addEventListener('click', function() {
     
     document.querySelector('.header-section h1').style.opacity = '1';
     
-    isButtonClicked = true;  // Set the flag to true
+    isButtonClicked = true;
 });
-
-const squares = document.querySelectorAll('.hover-square');
-console.log(`Found ${squares.length} squares.`);
-
-squares.forEach(square => {
-    square.addEventListener('mouseover', function() {
-        if (isButtonClicked) {  // Check if the button has been clicked
-            console.log('Mouseover event triggered.');
-            square.style.transform = 'translate(-50%, -50%) scale(1.1)';
-        }
-    });
-
-    square.addEventListener('mouseout', function() {
-        if (isButtonClicked) {  // Check if the button has been clicked
-            console.log('Mouseout event triggered.');
-            square.style.transform = 'translate(-50%, -50%) scale(1)';
-        }
-    });
-});
-
-
-
-
-
-
-
-
-
-
-
-
