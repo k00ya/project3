743d0582ff6dde88ae728d4b951b229b3d21e0e5
k00ya <133164751+k00ya@users.noreply.github.com>
2023-09-21 18:43:02 -0700

    fixed bug where background would already change before clicking get started button

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+let isButtonClicked = false;  // Initialize a flag variable
+
 window.onload = function() {
     alert("Congratulations! You won $6.0 million dollars!");
 };
@@ -13,80 +15,70 @@ const videos = {
 ['square1', 'square2', 'square3'].forEach((squareId, index) => {
     const square = document.getElementById(squareId);
     const audio = document.getElementById(`audio${index + 1}`);
-    
-    const video = videos[squareId]; // Select the appropriate video
+    const video = videos[squareId];
 
-    // Ensure volume is set to maximum for debugging
-    audio.volume = 0.3;
-
-    // For the video
     square.addEventListener('mouseover', function() {
-        Object.values(videos).forEach(v => { // Reset all videos
-            v.style.opacity = '0';
-            v.pause();
-        });
-        video.style.opacity = '1'; // Play the selected video
-        video.play();
-
-        // For the audio
-        let playPromise = audio.play();
-
-        if (playPromise !== undefined) {
-            playPromise.then(_ => {
-                // Automatic playback started!
-            }).catch(error => {
-                // Auto-play was prevented.
-                console.error("Playback failed: ", error);
+        if (isButtonClicked) {  // Check if the button has been clicked
+            Object.values(videos).forEach(v => {
+                v.style.opacity = '0';
+                v.pause();
             });
+            video.style.opacity = '1';
+            video.play();
+            
+            let playPromise = audio.play();
+            
+            if (playPromise !== undefined) {
+                playPromise.then(_ => {
+                }).catch(error => {
+                    console.error("Playback failed: ", error);
+                });
+            }
         }
     });
-    
+
     square.addEventListener('mouseout', function() {
-        video.style.opacity = '0';
-        video.pause();
-        
-        // For the audio
-        audio.pause();
-        audio.currentTime = 0;
+        if (isButtonClicked) {  // Check if the button has been clicked
+            video.style.opacity = '0';
+            video.pause();
+
+            audio.pause();
+            audio.currentTime = 0;
+        }
     });
 });
 
-
-
-
-
-
-// Fade out the summary content and make the squares and header visible upon button click
 document.getElementById('glow-on-hover').addEventListener('click', function() {
-    // Fade out the summary content
     document.querySelector('.summary-content').classList.add('fade-out');
     
-    // Make the squares visible
     document.querySelectorAll('.hover-square').forEach(square => {
         square.style.opacity = '1';
     });
     
-    // Make the header visible
     document.querySelector('.header-section h1').style.opacity = '1';
+    
+    isButtonClicked = true;  // Set the flag to true
 });
 
-// Fetch all squares from the DOM
 const squares = document.querySelectorAll('.hover-square');
-console.log(`Found ${squares.length} squares.`); // Log the number of squares found
+console.log(`Found ${squares.length} squares.`);
 
 squares.forEach(square => {
-    // Mouseover event to scale up
     square.addEventListener('mouseover', function() {
-        console.log('Mouseover event triggered.');
-        square.style.transform = 'translate(-50%, -50%) scale(1.1)';
+        if (isButtonClicked) {  // Check if the button has been clicked
+            console.log('Mouseover event triggered.');
+            square.style.transform = 'translate(-50%, -50%) scale(1.1)';
+        }
     });
- 
-    // Mouseout event to revert to original scale
+
     square.addEventListener('mouseout', function() {
-        console.log('Mouseout event triggered.');
-        square.style.transform = 'translate(-50%, -50%) scale(1)';
+        if (isButtonClicked) {  // Check if the button has been clicked
+            console.log('Mouseout event triggered.');
+            square.style.transform = 'translate(-50%, -50%) scale(1)';
+        }
     });
 });
+
 
 
 
